23d9ba11f44a0f824c4c4e6a0b4e329b8364731a
Kimhouy HOR <kimhouy.hor@allweb.com.kh>
2024-06-04 18:35:24 +0700

    about page

--- quiz-frontend/app/about/page.tsx
+++ quiz-frontend/app/about/page.tsx
@@ -3,9 +3,15 @@
 const About = () => {
     return (
         <section className="h-full">
-            <div className="flex min-h-full flex-col items-center justify-between p-24">            
-                <h1>About</h1>
-             
+            <div className="min-h-full p-24 flex flex-col gap-4">            
+                <div className="text-2xl px-4 py-2 mx-auto border border-green-600 bg-green-500/50 rounded-md">Welcome to Quiz Application</div>
+                
+                <div className="px-4 py-2 mx-auto border border-cyan-600 bg-cyan-500/50 rounded-md">
+                    <ul className="list-disc">
+                       Our main goal :&nbsp;
+                        <li className="ml-6">Develop a new and easy quiz system for education of general people</li>
+                    </ul>
+                </div>
             </div>
         </section>
         
--- quiz-frontend/app/admin/users/page.tsx
+++ quiz-frontend/app/admin/users/page.tsx
@@ -4,7 +4,7 @@ export default function User() {
     
     return (
         <div>
-            <DataTable fetchTable={}/>
+            {/* <DataTable fetchTable={}/> */}
         </div>
     )
 }
