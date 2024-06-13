569ce3ce5949bbc7f31a75d1d9cd9b2efc721c71
Kimhouy HOR <kimhouy.hor@allweb.com.kh>
2024-06-13 19:25:18 +0700

    new Dropdown for navation panel

--- quiz-frontend/components/navbar/nav-link.tsx
+++ quiz-frontend/components/navbar/nav-link.tsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 import { usePathname } from "next/navigation";
 import HandLogout from "@/utils/Actions/Auth/HandleLogout";
 import { useState } from "react";
+import Navdropdown from "./nav-dropdown";
 
 const NavLink = ({isUserLogin, userRole}: {isUserLogin:boolean, userRole:String} ) => {
 
@@ -18,7 +19,10 @@ const NavLink = ({isUserLogin, userRole}: {isUserLogin:boolean, userRole:String}
         <>           
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">                
                 <li>
-                    {userRole === "ROLE_ADMIN" &&
+                    { userRole === "ROLE_ADMIN" && <Navdropdown/>}
+
+
+                    {/* {userRole === "ROLE_ADMIN" &&
                         <button onClick={toggleDropdown} data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
                             Manage 
                             <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
@@ -26,7 +30,7 @@ const NavLink = ({isUserLogin, userRole}: {isUserLogin:boolean, userRole:String}
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
                             </svg>
                         </button>
-                    }
+                    } */}
                     {openDropdown && (
                         <div id="dropdownNavbar" className="absolute z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                             <ul className="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
