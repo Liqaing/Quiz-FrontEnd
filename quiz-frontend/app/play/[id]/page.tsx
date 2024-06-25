6050e5d2040a0486a6f1a8f2d93317ecde328593
Rithy <huotrithy01@gmail.com>
2024-06-25 22:50:20 +0700

    Display play history

--- quiz-frontend/app/play/[id]/page.tsx
+++ quiz-frontend/app/play/[id]/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import PlayHistoryView from "@/components/View/play/PlayHistoryView";
 import { PlayHistory } from "@/type/type";
 import { FetchOnePlay } from "@/utils/API/play/fetchOnePlay";
 import Link from "next/link";
@@ -8,11 +9,15 @@ import { useEffect, useState } from "react";
 
 export default function ViewPlay({ params }: { params: { id: string } }) {
 
-    const [playHistory, setPlayHistory] = useState<PlayHistory>();
+    const [playHistory, setPlayHistory] = useState<PlayHistory | null>(null);
+    const [date, setDate] = useState<Date>();
+
 
     async function fetchPlayHistory() {
         const data = await FetchOnePlay(params.id);
         if (data) {
+            const createDate = new Date(data.createdAt);
+            setDate(createDate);
             setPlayHistory(data);
         }
     } 
@@ -24,12 +29,31 @@ export default function ViewPlay({ params }: { params: { id: string } }) {
     return (
         <section className="sm:px-12 xs:px-6">                    
             <div className="max-w-screen-xl mx-auto p-2 mt-5">
+                    
+                <div className="flex justify-between border-b-2 pb-4">
+                    <div className="w-10/12">
+                        <p className="lg:text-lg md:text-base font-semibold uppercase text-gray-900 dark:text-white">{playHistory?.quizName}</p>
+                        <p className="lg:text-md md:text-sm my-1 text-[0.8rem] hyphens-auto font-extralight">
+                            {playHistory?.quizDescription}
+                        </p>
+                    </div>
+                    <div className="w-2/12">
+                        {                     
+                            date && (
+                                <p className="text-end lg:text-md md:text-sm">                            
+                                    Created - {((date?.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + date.getFullYear()}                
+                                </p>
+                            )
+                        }
+                    </div>
+                </div>
                 
-                {/* <Link          
-                    href="/admin/quiz" type="button" 
-                    className="ml-1 py-2.5 px-4 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
-                    Back
-                </Link> */}
+                <div className="mt-4">
+                    <p className="lg:text-base md:text-sm font-semibold uppercase text-gray-900 dark:text-white">Total Score : {playHistory?.score} / {playHistory?.answered.length}</p>
+
+                        
+                    <PlayHistoryView data={playHistory}></PlayHistoryView>
+                </div>
             </div>            
         </section>
     )
---
+++ quiz-frontend/components/View/play/PlayHistoryView.tsx
@@ -0,0 +1,61 @@
+import { PlayAnswer, PlayHistory, PlayQuestion } from "@/type/type";
+import { Button } from "@headlessui/react";
+
+export default function PlayHistoryView(props: {data: PlayHistory | null}) {
+
+
+    return (
+        <div className="w-full">
+            {
+                props.data?.answered.map((d:PlayQuestion, index:any) => {
+                    return (
+                        <div key={index} className="md:h-auto py-2 px-4 my-2 flex flex-col bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <div className="flex justify-between text-sm">
+                                <p className="py-1">{d.type}</p>
+
+                                <div className="flex gap-2">
+                                    <p className="p-1 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                        1 points
+                                    </p>                                 
+                                </div>
+                            </div>
+
+                            <div className="w-4/5">
+                                <p className="lg:text-lg md:text-base font-medium tracking-tight text-gray-900 dark:text-white">{`${index + 1}. ${d.question}`}</p>
+                            </div>
+                            
+                            <div className="grid grid-cols-2 gap-2 my-3">
+                                {
+                                    d.answers.map((answer:PlayAnswer, answerIndex:any) => {
+                                        return (
+                                            <p key={answerIndex} className={`${answer.pick ? answer.correct ? "bg-green-700/50 dark:bg-green-600/50" : "bg-red-700/50 dark:bg-red-600/50" : ""} px-3 py-2 rounded-lg w-fit text-white inline-flex justify-start items-center text-sm font-medium focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700`}>                                                
+                                                {
+                                                    answer.correct ? 
+                                                    (                                                        
+                                                        <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="#21e600" viewBox="0 0 20 20">
+                                                            <path d="M16.707 5.293a1 1 0 0 0-1.414 0L8 12.586 4.707 9.293a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.414 0l8-8a1 1 0 0 0 0-1.414z"/>
+                                                        </svg>
+                                                    )
+                                                    :
+                                                    (
+                                                        <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 20 20">
+                                                            <path d="M10 8.586L3.293 1.879A1 1 0 0 0 1.879 3.293L8.586 10l-6.707 6.707a1 1 0 0 0 1.414 1.414L10 11.414l6.707 6.707a1 1 0 0 0 1.414-1.414L11.414 10l6.707-6.707a1 1 0 1 0-1.414-1.414L10 8.586z"/>
+                                                        </svg>
+                                                    )
+                                                }
+                                                {answer.answer}                                                
+                                            </p>
+                                        )
+                                        
+                                    })
+                                }
+                                
+                            </div>                            
+                        </div>             
+                    )
+                })
+            }
+            
+        </div>
+    )
+}
