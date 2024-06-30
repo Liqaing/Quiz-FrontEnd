f7f37cd2bd54cfe9577a6dfad6c1360b7f19b884
Rithy <huotrithy01@gmail.com>
2024-06-30 13:00:31 +0700

    Update quiz title

--- quiz-frontend/components/navbar/nav-content.tsx
+++ quiz-frontend/components/navbar/nav-content.tsx
@@ -15,7 +15,7 @@ const NavBarContent = async () => {
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <img src="/icon.png" className="h-8" alt="Quiz Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Quiz</span>
+                        <span className="self-center text-2xl font-normal whitespace-nowrap dark:text-white">អ្នកនឹងក្លាយជាសេដ្ឋី</span>
                     </Link>
                     <button id="nav-menu-toggle" data-collapse-toggle="navbar-menu" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
