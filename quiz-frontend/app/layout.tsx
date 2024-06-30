6032d557e86b847c9a8d8fb6484e78064a111bce
Kimhouy HOR <kimhouy.hor@allweb.com.kh>
2024-06-30 18:01:45 +0700

    otp register and about page

--- quiz-frontend/app/layout.tsx
+++ quiz-frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/navbar";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} relative`}>
         <Navbar/>
+        <Toaster position="bottom-right" visibleToasts={1}/>
         {children}
         </body>
     </html>
--- quiz-frontend/utils/data.ts
+++ quiz-frontend/utils/data.ts
@@ -13,3 +13,109 @@ export const order = {
 export type FormState = {
   message: string;
 };
+
+
+export const member = [
+  {
+      name: "Hor KimHouy",
+      position: "Team Leader",
+      image: "/kimhouy.jpg",
+  },
+  {
+      name: "Hout Rithy",
+      position: "Backup Leader",
+      image: "/rithy.jpg",
+  },
+  {
+      name: "Vorn Navatra",
+      position: "Team Member",
+      image: "/defaultProfile.bmp",
+  },
+  {
+      name: "Oem YongSinh",
+      position: "Team Member",
+      image: "/sinh.jpg",
+  },
+  {
+      name: "Heng KeaHak",
+      position: "Team Member",
+      image: "/keahak.jpg",
+  },
+  {
+      name: "Um Sethey",
+      position: "Team Member",
+      image: "/sethey.jpg",
+  },{
+      name: "Ol Eevin",
+      position: "Team Member",
+      image: "/eevin.jpg",
+  }
+]
+
+export const roleMember = [
+  {
+      title: "Backend Development",
+      team : [
+          {
+              name: "Hor KimHouy",
+              position: "Backend Developer",
+              image: "/kimhouy.jpg",
+          },
+          {
+              name: "Hout Rithy",
+              position: "Backend Support",
+              image: "/rithy.jpg",
+          },
+      ]
+  },
+  {
+      title: "Frontend Development",
+      team : [
+          {
+              name: "Hout Rithy",
+              position: "Frontend Developer",
+              image: "/rithy.jpg",
+          },
+          {
+              name: "Hor KimHouy",
+              position: "Frontend Support",
+              image: "/kimhouy.jpg",
+          },
+      ]
+  },
+  {
+      title: "Testing",
+      team : [
+          {
+              name: "Vorn Navatra",
+              position: "API Tester",
+              image: "/defaultProfile.bmp",
+          },
+          {
+              name: "Ol Eevin",
+              position: "API Tester",
+              image: "/eevin.jpg",
+          },
+          {
+              name: "Oem YongSinh",
+              position: "Documentation Specialist",
+              image: "/sinh.jpg",
+          },
+      ]
+  },
+  {
+      title: "Presentation",
+      team : [
+          {
+              name: "Heng KeaHak",
+              position: "PowerPoint Designer",
+              image: "/keahak.jpg",
+          },
+          {
+              name: "Um Sethey",
+              position: "PowerPoint Designer",
+              image: "/sethey.jpg",
+          },
+      ]
+  },
+]
